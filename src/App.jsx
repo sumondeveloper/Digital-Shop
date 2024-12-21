f211a54f887601b75b88a3d334930f1238de0895
sumondeveloper <learnwithsumon@gmail.com>
2024-12-21 11:43:37 +0600

    footer section add to the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 };
